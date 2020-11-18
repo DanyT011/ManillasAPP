ef13be340a57b7f83db9087e137bb3e6db7000bf
Daniela Toncel Molina <tonceld@gmail.com>
2020-11-17 23:04:29 -0500

    Método cotizar y ajustes a layout

--- app/src/main/java/com/example/manillasapp/MainActivity.java
+++ app/src/main/java/com/example/manillasapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.manillasapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -48,4 +50,74 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, monedas);
         moneda.setAdapter(adapter);
     }
+
+    public void cotizar(View v){
+        double precio = 0;
+        int mon, mat, dij, tip, cant = 1, dolar = 3200;
+
+        cant = Integer.parseInt(cantidad.getText().toString());
+        mon = moneda.getSelectedItemPosition();
+        mat = material.getSelectedItemPosition();
+        dij = dije.getSelectedItemPosition();
+        tip = tipo.getSelectedItemPosition();
+
+        switch (mon){
+            case 0:
+                if(mat == 0 && dij == 0 && (tip == 0 || tip == 1)){
+                    precio = 100 * cant;
+                }else if(mat == 0 && dij == 0 && tip == 2){
+                    precio = 80 * cant;
+                }else if (mat == 0 && dij == 0 && tip == 3){
+                    precio = 70 * cant;
+                }else if (mat == 0 && dij == 1 && (tip == 0 || tip == 1)){
+                    precio = 120 * cant;
+                }else if (mat == 0 && dij == 1 && tip == 2){
+                    precio = 100 * cant;
+                }else if (mat == 0 && dij == 1 && tip == 3){
+                    precio = 90 * cant;
+                }else if(mat == 1 && dij == 0 && (tip == 0 || tip == 1)){
+                    precio = 90 * cant;
+                }else if(mat == 1 && dij == 0 && tip == 2){
+                    precio = 70 * cant;
+                }else if (mat == 1 && dij == 0 && tip == 3){
+                    precio = 50 * cant;
+                }else if (mat == 1 && dij == 1 && (tip == 0 || tip == 1)){
+                    precio = 110 * cant;
+                }else if (mat == 1 && dij == 1 && tip == 2){
+                    precio = 90 * cant;
+                }else if (mat == 1 && dij == 1 && tip == 3){
+                    precio = 80 * cant;
+                }
+                break;
+            case 1:
+                if(mat == 0 && dij == 0 && (tip == 0 || tip == 1)){
+                    precio = (100 * dolar) * cant;
+                }else if(mat == 0 && dij == 0 && tip == 2){
+                    precio = (80 * dolar) * cant;
+                }else if (mat == 0 && dij == 0 && tip == 3){
+                    precio = (70 * dolar) * cant;
+                }else if (mat == 0 && dij == 1 && (tip == 0 || tip == 1)){
+                    precio = (120 * dolar) * cant;
+                }else if (mat == 0 && dij == 1 && tip == 2){
+                    precio = (100 * dolar) * cant;
+                }else if (mat == 0 && dij == 1 && tip == 3){
+                    precio = (90 * dolar) * cant;
+                }else if(mat == 1 && dij == 0 && (tip == 0 || tip == 1)){
+                    precio = (90 * dolar) * cant;
+                }else if(mat == 1 && dij == 0 && tip == 2){
+                    precio = (70 * dolar) * cant;
+                }else if (mat == 1 && dij == 0 && tip == 3){
+                    precio = (50 * dolar) * cant;
+                }else if (mat == 1 && dij == 1 && (tip == 0 || tip == 1)){
+                    precio = (110 * dolar) * cant;
+                }else if (mat == 1 && dij == 1 && tip == 2){
+                    precio = (90 * dolar) * cant;
+                }else if (mat == 1 && dij == 1 && tip == 3){
+                    precio = (80 * dolar) * cant;
+                }
+                break;
+        }
+        resultado.setText(String.valueOf(precio));
+    }
 }
+
